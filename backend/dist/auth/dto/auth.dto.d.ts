a89a80ac38d3a3f0c6b2479d4f0a9df46cd14a12
Mathieu <mlecherb@c4r2p4.42nice.fr>
2023-02-27 16:30:55 +0100

    Dist rm

--- backend/dist/auth/dto/auth.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class AuthDto {
-    id: number;
-    email: string;
-    login: string;
-    first_name: string;
-    last_name: string;
-    img_link: string;
-}
--- backend/dist/user/interface/userSocket.interface.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "@prisma/client";
-export interface UsersSocketInterface {
-    user: User;
-    client: any;
-}

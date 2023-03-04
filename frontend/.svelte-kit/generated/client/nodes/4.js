82a1609cd7f9f131cfd24fa03fce47115ff5f938
julien-ctx <jdaodkql@gmail.com>
2023-03-04 17:40:31 +0100

    remove svelte-kit

--- frontend/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/fakeuser/+page.svelte";
--- frontend/.svelte-kit/generated/client/nodes/9.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/proto/+page.svelte";

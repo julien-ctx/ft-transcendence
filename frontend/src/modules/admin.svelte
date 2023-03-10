590b46d12d01ba7e8b16fb6689f259d504e48810
Mathieu <math.lecherbonnier@gmail.com>
2023-03-10 15:23:22 +0100

    Merge OP to master

--- frontend/src/modules/admin.svelte
+++ frontend/src/modules/admin.svelte
@@ -9,15 +9,24 @@
 	import Close from './htmlComponent/svgComponent/svgClose.svelte';
 	import Door from './htmlComponent/svgComponent/svgDoor.svelte';
 	import Upgrade from './htmlComponent/svgComponent/svgUpgrade.svelte';
+
 	export let socket : any;
 	export let room : string;
 	export let infoChannel : any;
 	let members : any = [];
-	let current : any;
+	let current : any = {};
+
+	// Change password
+	let currentPass : string = '';
+	let newPass : string = '';
+	let newPassConfirm : string = '';
+	let validatePass : Boolean = false;
+	let passError : string = '';
+
 
 	onMount(async () => {
 		current = infoChannel.filter((Chan : any) => Chan.name === room)[0];
-		console.log('Current ->', current.owner);
+		// console.log('Current ->', current);
 		try {
 			// console.log(room);
 			await axios.get(`${API_URL}/Chat/getMembers/${room}`, {
@@ -32,17 +41,39 @@
 		} catch (error) {
 			console.log(error);
 		}
-		console.log('Members ->', members)
+		// console.log('Members ->', members)
 
 		socket.on('deletedMember', (data : any) => {
-			console.log('Data ->', data);
-			members.filter((mem : any) => mem.user.id_user === data.id_user);
-			console.log('New members ->', members);
+			let newMembers = [];
+			for (let i = 0; i < members.length; i++) {
+				if (members[i].user.id_user !== data.id_user) {
+					newMembers.push(members[i]);
+				}
+			}
+			members = newMembers;
+		});
+
+		socket.on('successVerify', () => {
+			validatePass = true;
+		});
+
+		socket.on('newRight', (data : any) => {
+			// console.log('New right ->', {data});
+			members = members.map((mem : any) => {
+				if (mem.user.id_user === data.id_user) {
+					mem.admin = data.admin;
+				}
+				return mem;
+			});
+			members = members;
+			for (let i = 0; i < members.lenght; i++) {
+
+			}
 		});
 	});
 
 	function admin(sanction : string, Punished : any) {
-		console.log(sanction);
+		// console.log(sanction);
 		members.filter((mem : any) => mem.user.login !== Punished);
 		socket.emit('sanction', {
 			roomName: room,
@@ -70,10 +101,34 @@
 		});
 	}
 
+	function DEOP(Punished : any) {
+		socket.emit('DEOP', {
+			roomName: room,
+			member: Punished,
+		});
+	}
+
 	let selected = '';
-	// let sanction = '';
 	let time = '';
 	let duration = '';
+
+	function verifPassword(event : KeyboardEvent) {
+		// console.log('test');
+		if (event.key == "Enter" && currentPass !== '') 
+			socket.emit('verifPassword', {roomName: room, password: currentPass});
+	}
+
+	function changePass() {
+		passError = '';
+		if (newPass !== newPassConfirm ) {
+			passError = "Passwords don't match";
+			newPass = '';
+			newPassConfirm = '';
+		} else if (newPass !== '' && newPassConfirm !== '') {
+			socket.emit('changePass', {roomName: room, Pass: newPass, Cpass : newPassConfirm});
+		}
+	}
+	
 </script>
 
 
@@ -86,7 +141,7 @@
 					<div class="bg-light_yellow rounded border">
 						Admin
 					</div>
-				{:else if member.ower}
+				{:else if member.owner === true}
 					<div class="!bg-light_blue rounded border">
 						Owner
 					</div>
@@ -103,6 +158,19 @@
 								<div class="flex justify-center">
 									<button on:click={() => OP(member.user)}>
 										<div>OP {member.user.login}</div>
+									</button>
+								</div>
+							</DropdownItem>
+						</Dropdown>
+					{:else}
+						<div class="transform rotate-180">
+							<Upgrade />
+						</div>
+						<Dropdown class="w-full">
+							<DropdownItem>
+								<div class="flex justify-center">
+									<button on:click={() => DEOP(member.user)}>
+										<div>DE-OP {member.user.login}</div>
 									</button>
 								</div>
 							</DropdownItem>
@@ -212,5 +280,27 @@
 				{/if}
 			</div>
 		{/each}
+	</div>
+{:else}
+	<div class="flex justify-center">
+		There is no members
+	</div>
+{/if}
+{#if current.status === 'Protected'}
+	<div class="flex justify-center">Change password</div>
+	<div class="flex flex-col">
+		{#if validatePass === false}
+			<input type="password" placeholder="Current password" bind:value={currentPass} on:keydown={verifPassword}>
+			<div class="border-t"></div>
+		{:else}
+			<input type="password" placeholder="New password" bind:value={newPass}>
+			<input type="password" placeholder="Confirm password" bind:value={newPassConfirm}>
+			{#if passError !== ''}
+				<p class="flex justify-center text-red-500 text-sm">{passError}</p>
+			{/if}
+			<div class="flex justify-center">
+				<button on:click={() => changePass()}>Change password</button>
+			</div>
+		{/if}
 	</div>
 {/if}
